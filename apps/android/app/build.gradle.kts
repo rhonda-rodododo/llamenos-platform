3dcae91dc02a19fe52b8b5914a9e72dec595a45a
llamenos-auto <333950803+llamenos-auto@users.noreply.github.com>
2026-09-26 06:10:18 +0000

    chore: prepare release 0.19.17

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.19.16"
+        versionName = "0.19.17"
 
         testInstrumentationRunner = "org.llamenos.hotline.CucumberHiltRunner"
 
